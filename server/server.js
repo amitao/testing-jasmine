974debf940fa5b5efd32fbbc2a7eb66537db25fc
Amie Thao <amie.c.thao@gmail.com>
2019-04-16 17:07:33 -0500

    add test case

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.use('GET', (req, res) => {
+app.get('/', (req, res) => {
   res.send("Testing with Jasmine");
-})
+});
 
 
 app.listen( PORT, () => {
   console.log(`Listening to port ${PORT}`);
-})
+});
